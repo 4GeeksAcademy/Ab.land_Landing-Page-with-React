1f028795ab41c82e9b44e7e8d9308aec63c72b88
Ablandaeta <ablandaeta@gmail.com>
2025-04-21 17:46:55 +0000

    V2.0

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -3,21 +3,21 @@ import React from 'react'
 const Card = (props) => {
 
     return (
-
-        <div className="card col-10 offset-1 col-sm-8 offset-sm-2 col-md-5 offset-md-0 offset-xl-0 col-xl-3  p-0 my-2 ms-md-5 ms-xl-0" >
-            <img style={{height:"225px",width:"auto", objectFit: "cover"}}
-             src={props.imgSrc}
-            className="card-img-top " alt={props.imgAlt}/>
+        <div className='col-10 col-sm-8 col-md-5 col-xl-3'>
+            <div className="card" >
+                <img style={{ height: "225px", objectFit: "cover" }}
+                    src={props.imgSrc}
+                    className="card-img-top " alt={props.imgAlt} />
                 <div className="card-body">
-                    <h3 className="card-title text-center ">{props.cardTitle}</h3>
-                    <p className="card-text">{props.cardContent} </p>
-                    <p className="card-text"><small className="text-body-secondary">{props.cardDate}</small></p>
+                    <h3 className="card-title text-center ">{props.title}</h3>
+                    <p className="card-text">{props.content} </p>
+                    <p className="card-text"><small className="text-body-secondary">{props.lastUpdate}</small></p>
                 </div>
                 <div className='card-footer text-center'>
-                    <a href={props.aLink} target="blank" className='btn btn-primary'>Find Out More</a>
+                    <a href={props.anchorLink} target="blank" className='btn btn-primary'>Find Out More</a>
                 </div>
+            </div>
         </div>
-
     )
 };
 
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -9,56 +9,67 @@ import Card from "./Card";
 
 //create your first component
 const Home = () => {
+
+	let cardProps = [
+		{
+			imgSrc: "https://images.unsplash.com/photo-1744361448609-c5d3417f00ce",
+			imgAlt: "a flaying seagull",
+			title: "Card 1",
+			content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor quasi doloremque perferendis, facere dolorum eaque aliquam sapiente. Eius, et. Corporis dolore impedit atque reprehenderit fuga autem unde similique sint non!",
+			lastUpdate: "Last updated 10s",
+			anchorLink: "https://unsplash.com/photos/a-seagull-soars-over-the-wavy-ocean-Sh8VAhKJeu4"
+
+		},
+		{
+			imgSrc: "https://images.unsplash.com/photo-1678811116814-26372fcfef1b",
+			imgAlt: "A ligth halo in a frozzen landscape",
+			title: "Card 2",
+			content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor quasi doloremque perferendis, facere dolorum eaque aliquam sapiente. Eius, et. Corporis dolore impedit atque reprehenderit fuga autem unde similique sint non!",
+			lastUpdate: "Last updated 7m",
+			anchorLink: "https://unsplash.com/photos/a-person-riding-skis-on-top-of-a-snow-covered-slope-Jfzke0ie6RU"
+
+		},
+		{
+			imgSrc: "https://images.unsplash.com/vector-1744384271002-ed577f272ed0",
+			imgAlt: "an ilustration of a man holding a torch in the dark",
+			title: "Card 3",
+			content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor quasi doloremque perferendis, facere dolorum eaque aliquam sapiente. Eius, et. Corporis dolore impedit atque reprehenderit fuga autem unde similique sint non!",
+			lastUpdate: "Last updated 9m",
+			anchorLink: "https://unsplash.com/illustrations/a-person-is-holding-a-torch-in-a-dark-forest-33my-dJZ3N4"
+
+		},
+		{
+			imgSrc: "https://images.unsplash.com/photo-1743247975472-611e63868a1a",
+			imgAlt: "top to bottom view of a stairway",
+			title: "Card 4",
+			content: "hola hello hallo doiLorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor quasi doloremque perferendis, facere dolorum eaque aliquam sapiente. Eius, et. Corporis dolore impedit atque reprehenderit fuga autem unde similique sint non!",
+			lastUpdate: "Last updated 7h",
+			anchorLink: "https://unsplash.com/photos/escalators-in-a-futuristic-yellow-walled-building-f9VeYakvLiI"
+
+		}
+	]
+
+
+
+
 	return (
 		<>
 			<Navbar />
-			<div className="container">
-				<Jumbo/>
-				<div className="row container">
-				
-					
-					<Card 
-					aLink="https://unsplash.com/photos/a-seagull-soars-over-the-wavy-ocean-Sh8VAhKJeu4"
-					cardDate="Last updated 10s"
-					cardContent= "Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-					Dolor quasi doloremque perferendis, facere dolorum eaque aliquam sapiente. 
-					Eius, et. Corporis dolore impedit atque reprehenderit fuga autem unde similique sint non!"
-					cardTitle= "Card 1"
-					imgAlt= "a flaying seagull"
-					imgSrc="https://images.unsplash.com/photo-1744361448609-c5d3417f00ce"
-					/>
-					<Card 
-					aLink="https://unsplash.com/photos/a-person-riding-skis-on-top-of-a-snow-covered-slope-Jfzke0ie6RU"
-					cardDate="Last updated 1m"
-					cardContent= "Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-					Dolor quasi doloremque perferendis, facere dolorum eaque aliquam sapiente. 
-					Eius, et. Corporis dolore impedit atque reprehenderit fuga autem unde similique sint non!"
-					cardTitle= "Card 2"
-					imgAlt= "A ligth halo in a frozzen landscape"
-					imgSrc="https://images.unsplash.com/photo-1678811116814-26372fcfef1b"
-					/>
-					<Card 
-					aLink="https://unsplash.com/illustrations/a-person-is-holding-a-torch-in-a-dark-forest-33my-dJZ3N4"
-					cardDate="Last updated 9m"
-					cardContent= "Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-					Dolor quasi doloremque perferendis, facere dolorum eaque aliquam sapiente. 
-					Eius, et. Corporis dolore impedit atque reprehenderit fuga autem unde similique sint non!"
-					cardTitle= "Card 1"
-					imgAlt= "an ilustration of a man holding a torch in the dark"
-					imgSrc="https://images.unsplash.com/vector-1744384271002-ed577f272ed0"
-					/>
-					<Card 
-					aLink="https://unsplash.com/photos/escalators-in-a-futuristic-yellow-walled-building-f9VeYakvLiI"
-					cardDate="Last updated 7h"
-					cardContent= "Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-					Dolor quasi doloremque perferendis, facere dolorum eaque aliquam sapiente. 
-					Eius, et. Corporis dolore impedit atque reprehenderit fuga autem unde similique sint non!"
-					cardTitle= "Card 4"
-					imgAlt= "top to bottom view of a stairway"
-					imgSrc="https://images.unsplash.com/photo-1743247975472-611e63868a1a"
-					/>
+			<div className="container ">
+				<Jumbo />
+				<div className="row container g-3">
+					{cardProps.map((value) => {
+						return <Card
+							imgSrc={value.imgSrc}
+							imgAlt={value.imgAlt}
+							title={value.title}
+							content={value.content}
+							lastUpdate={value.lastUpdate}
+							anchorLink={value.anchorLink}
+						/>
+						})
+					}
 				</div>
-
 			</div>
 			<Footer />
 
